5757cf9586673e1d96254102504f08f8cbb1e7a3
Dmytro Kriuk <dmytro.kryuk@gmail.com>
2021-01-27 21:28:59 +0200

    feat: add section start

---
+++ Quantum-projects-testTask/js/script.js
@@ -0,0 +1,30 @@
+/*
+const selectSingle = document.querySelector('.__select');
+const selectSingle_title = selectSingle.querySelector('.__select__title');
+const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
+
+// Toggle menu
+selectSingle_title.addEventListener('click', () => {
+  if ('active' === selectSingle.getAttribute('data-state')) {
+    selectSingle.setAttribute('data-state', '');
+  } else {
+    selectSingle.setAttribute('data-state', 'active');
+  }
+});
+
+// Close when click to option
+for (let i = 0; i < selectSingle_labels.length; i++) {
+  selectSingle_labels[i].addEventListener('click', (evt) => {
+    selectSingle_title.textContent = evt.target.textContent;
+    selectSingle.setAttribute('data-state', '');
+  });
+}
+*/
+
+
+let searchSelect = document.querySelector('.btn-lang__title');
+let eachSelect = document.querySelector('.categories-search')
+searchSelect.addEventListener("click", function (e) {
+  searchSelect.classList.toggle('_active');
+  eachSelect.classList.toggle('_active');
+});
