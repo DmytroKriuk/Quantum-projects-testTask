1f7856f8a9d2f8744d642fc564a59290df3cdc23
Dmytro Kriuk <dmytro.kryuk@gmail.com>
2021-01-29 08:07:41 +0200

    feat: add responsive and refactoring code

--- Quantum-projects-testTask/js/script.js
+++ Quantum-projects-testTask/js/script.js
@@ -36,3 +36,15 @@ if (document.querySelector('.people-slider')) {
   });
   
 }
+
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
