75d2e1a28f564f16fabda43ccf5020ec8fd8f9ba
Dmytro Kriuk <dmytro.kryuk@gmail.com>
2021-02-01 10:50:51 +0200

    fix: fix style.css

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import LoadScript from 'vue-plugin-load-script'
 import 'bootstrap'
-import Swiper from 'swiper';
-window.Swiper = Swiper; 
 Vue.use(LoadScript);
 
 Vue.config.productionTip = true
